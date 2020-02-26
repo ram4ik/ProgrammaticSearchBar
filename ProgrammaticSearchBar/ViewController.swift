69192a60ac088cf2f2e852aa14aac672db291815
Ramill Ibragimov <ramill_ibragimov@icloud.com>
2020-02-26 13:05:06 +0200

    Initial Commit

---
+++ ProgrammaticSearchBar/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ProgrammaticSearchBar
+//
+//  Created by ramil on 26.02.2020.
+//  Copyright © 2020 com.ri. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
