4e5a7454153aba38450c1e3197436c6c10d52b1a
Ramill Ibragimov <ramill_ibragimov@icloud.com>
2020-02-26 13:47:29 +0200

    Fully Programmatic Search Bar

--- ProgrammaticSearchBar/ViewController.swift
+++ ProgrammaticSearchBar/ViewController.swift
@@ -9,6 +9,8 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    let searchBar = UISearchBar()
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -16,12 +18,45 @@ class ViewController: UIViewController {
         configureUI()
     }
     
+    @objc func handleShowSearchBar() {
+        search(shouldShow: true)
+        searchBar.becomeFirstResponder()
+    }
+    
     func configureUI() {
         view.backgroundColor = .white
         
+        searchBar.sizeToFit()
+        searchBar.delegate = self
+        
         navigationController?.navigationBar.prefersLargeTitles = true
         navigationItem.title = "Search Bar"
+        navigationController?.navigationBar.barTintColor = UIColor(red: 55/255, green: 120/255, blue: 250/255, alpha: 1)
+        navigationController?.navigationBar.isTranslucent = false
+        navigationController?.navigationBar.barStyle = .black
+        navigationController?.navigationBar.tintColor = .white
+        showSearchBarButton(shouldShow: true)
+    }
+    
+    func showSearchBarButton(shouldShow: Bool) {
+        if shouldShow {
+            navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .search, target: self, action: #selector(handleShowSearchBar))
+        } else {
+            navigationItem.rightBarButtonItem = nil
+        }
+    }
+    
+    func search(shouldShow: Bool) {
+        showSearchBarButton(shouldShow: !shouldShow)
+        searchBar.showsCancelButton = shouldShow
+        navigationItem.titleView = shouldShow ? searchBar : nil
     }
 
 }
 
+extension ViewController: UISearchBarDelegate {
+    
+    func searchBarCancelButtonClicked(_ searchBar: UISearchBar) {
+         search(shouldShow: false)
+    }
+}
